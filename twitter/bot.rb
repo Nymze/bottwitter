09bd7a4976415c7607f807835de12d692e9cea63
Nymze <31897151+Nymze@users.noreply.github.com>
2018-01-17 19:13:55 +0000

    Mise en forme

--- twitter/bot.rb
+++ twitter/bot.rb
@@ -1,31 +1,48 @@
-
 require 'twitter'
 require 'pry'
 
 # quelques lignes qui enregistrent les clés d'APIs
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ""
-  config.consumer_secret     = ""
-  config.access_token        = ""
-  config.access_token_secret = ""
+  config.consumer_key        = "KEY"
+  config.consumer_secret     = "KEY"
+  config.access_token        = "KEY"
+  config.access_token_secret = "KEY"
 end
 p client
 
+# 1 er Fonction / Tweet Bonjour Monde : 
 
-# LIKE LES POST D'UNE LISTE DE NOMS 
+# client.update("Bonjour Monde !")
+
+
+
+# 2 eme Fonction / LIKE LES POST D'UNE LISTE DE NOMS 
 
 myArray = ["@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@seizar","@Elodie_legeay"] #array de noms
 
 myArray.each do |x|   			#pour chaque nom dans l'arret :
 tweets = client.user_timeline(x)#va chercher les tweets les plus recent par ID
 puts (tweets) 					#inscrit l'ID du tweet 
-client.favorite(tweets) 	#aime les tweets de la liste créée 
+client.favorite(tweets) 	#aime les tweets de la liste créée  tweets = ID du tweet
 end
+
+#Streaming de tweet
+
+client2 = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = "KEY"
+  config.consumer_secret     = "KEY"
+  config.access_token        = "KEY"
+  config.access_token_secret = "KEY"
+end
+p client
+
 
 =begin
 
-#scan les tweets avec le #ruby, prend les 3 derniers et leur tweet "@username : Hey I love Ruby too"
+# 3 eme fonction  / #scan les tweets avec le #ruby, prend les 3 derniers et leur tweet "@username : Hey I love Ruby too"
+
+
 client.search('#ruby').take(3).each do |tweet| 
  client.update("@#{tweet.user.screen_name}: Hey I love Ruby too :)")
 
@@ -33,18 +50,9 @@ end
 
 =end
 
-#Streaming de tweet
+# 4 eme Fonction : Stream les tweets avec coffee et tea in it  & les imprime dans la console.
 
-client2 = Twitter::Streaming::Client.new do |config|
-  config.consumer_key        = ""
-  config.consumer_secret     = ""
-  config.access_token        = ""
-  config.access_token_secret = ""
-end
-p client
-
-
-=begin --- Stream all tweets & RT with coffee and tea in it 
+=begin 
 
 topics = ["coffee", "tea"]
 client.filter(track: topics.join(",")) do |object|
@@ -53,11 +61,3 @@ end
 
 
 =end 
-
-
-
-
-
-
-
-
